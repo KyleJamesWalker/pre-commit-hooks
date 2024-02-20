fc0318ba97995c4d4a0795eca56ab2bdc4f1e7df
Kyle James Walker (he/him) <KyleJamesWalker@gmail.com>
2024-02-20 15:31:55 -0800

    Add todo pre-commit hook

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# pre-commit-hooks
-Custom hooks for pre-commit
+# Pre Commit Hooks
+
+This repository contains some pre-commit hooks for use with [pre-commit](https://pre-commit.com/).
+
+#### `todo`
+Ensures that TODO comments are removed, using the python re module to match lines.
+  - `--search-pattern <pattern>` - Change the default grep pattern. The default looks for a
+    ticket number directly after the TODO with: `^.*TODO:(?!\s*\[?[A-Z]{1,5}-\d+).*$`.
+  - `--repo-skip-pattern <pattern>` - Skip the checks when the repo name matches the
+    pattern, useful when allowing TODO comments in a `Template` repo.
+    Default: `.*-template-.*`
---
+++ todo.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+"""Pre-commit hook to error on TODOs without matching a ticket directly afterwards."""
+import argparse
+import os
+import re
+import sys
+
+
+def err(s: str) -> None:
+    """Print a string to stderr."""
+    print(s, file=sys.stderr)
+
+
+def main():
+    """Entry point for the pre-commit hook."""
+    matches = []
+    return_val = 0
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--search-pattern",
+        default=r"^.*TODO:(?!\s*\[?[A-Z]{1,5}-\d+).*$",
+    )
+    parser.add_argument("--repo-skip-pattern", default=".*-template-.*")
+    parser.add_argument("filenames", nargs="*")
+    args = parser.parse_args()
+
+    if re.search(args.repo_skip_pattern, os.getcwd().split(os.sep)[-1]):
+        # Skip the hook when requested for template repos.
+        pass
+    else:
+        matcher = re.compile(args.search_pattern)
+        for filename in args.filenames:
+            with open(filename, "r") as file:
+                for line_num, line in enumerate(file, start=1):
+                    if matcher.match(line):
+                        matches.append((filename, line_num, line.lstrip().rstrip()))
+        if matches:
+            err('Error: Search pattern found. Please remove or add tracking ticket.')
+            for match in matches:
+                err(f"  {match[0]}:{match[1]} - {match[2]}")
+            err('')
+            return_val = 1
+
+    return return_val
+
+
+if __name__ == "__main__":
+    sys.exit(main())
